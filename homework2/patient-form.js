cf1e78edbeb940ae1a05fa27e90d3aeaf7989e44
djo98769 <86978976+djo98769@users.noreply.github.com>
2026-03-25 18:05:40 -0500

    Update patient-form.js

--- homework2/patient-form.js
+++ homework2/patient-form.js
@@ -21,33 +21,29 @@ function getdata1() {
   var formoutput;
   var datatype;
   var i;
-  formoutput = "<table class='output'><th>Dataname</th><th>Type</th><th>Value</th>";
-  for (i = 0; i < formcontents.length; i++) {
-            console.log("item: "+i+" "+formcontents.elements[i].name+" = "+formcontents.elements[i].value);
-            /* if (formcontents.elements[i].value !="") { */
-              datatype = formcontents.elements[i].type;
-              switch (datatype) {
-                case "checkbox":
-                  if (formcontents.elements[i].checked) {
-                    formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
-                    formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
-                    formoutput = formoutput +"<td class='outputdata'>Checked</td></tr>";
-                  }
-                  break;
-               case "radio":
-                    if (formcontents.elements[i].checked) {
-                      formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
-                      formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
-                      formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
-                    }
-                  break;
-                case "button": case "submit": case "reset":
-                  break;
-                default:
-                  formoutput = formoutput + "<tr><td align='right'>"+formcontents.elements[i].name+"</td>";
-                  formoutput = formoutput +"<td align='right'>"+ datatype + "</td>";
-                  formoutput = formoutput +"<td class='outputdata'>"+ formcontents.elements[i].value+"</td></tr>";
-                }
+  formoutput = "<table class='output'><tr><th>Field Name</th><th>Your Entry</th></tr>";
+for (i = 0; i < formcontents.length; i++) {
+    datatype = formcontents.elements[i].type;
+    switch (datatype) {
+        case "checkbox":
+            if (formcontents.elements[i].checked) {
+                formoutput += "<tr><td align='right'>" + formcontents.elements[i].name + "</td>";
+                formoutput += "<td class='outputdata'>Checked</td></tr>";
+            }
+            break;
+        case "radio":
+            if (formcontents.elements[i].checked) {
+                formoutput += "<tr><td align='right'>" + formcontents.elements[i].name + "</td>";
+                formoutput += "<td class='outputdata'>" + formcontents.elements[i].value + "</td></tr>";
+            }
+            break;
+        case "button": case "submit": case "reset":
+            break;
+        default:
+            formoutput += "<tr><td align='right'>" + formcontents.elements[i].name + "</td>";
+            formoutput += "<td class='outputdata'>" + formcontents.elements[i].value + "</td></tr>";
+    }
+}
             /* } */
 
   }
