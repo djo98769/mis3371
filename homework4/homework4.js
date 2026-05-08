4a97d467d08d4a6fbb4857c824b5ae81d87c0089
djo98769 <86978976+djo98769@users.noreply.github.com>
2026-05-08 03:33:43 -0500

    Update homework4.js

--- homework4/homework4.js
+++ homework4/homework4.js
@@ -422,4 +422,17 @@ function loadAllLocalStorage() {
     });
 }
 
+let lastScrollTop = 0;
+const header = document.getElementById("header");
+
+window.onscroll = function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop && scrollTop > 100) {
+        header.classList.add("header-hidden");
+    } else {
+        header.classList.remove("header-hidden");
+    }
+    lastScrollTop = scrollTop;
+};
     /* End of document: homework4.js */
